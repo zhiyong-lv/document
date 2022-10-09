794659f0584d6a26909b7ae8579134d94f760519
zhiyong-lv <zhiyong_lv1983@outlook.com>
2022-10-09 09:26:41 +0800

    add PriorityBlockingQueue related analysis

--- src/test/java/com/lzy/learning/demo/PriorityBlockingQueueTest.java
+++ src/test/java/com/lzy/learning/demo/PriorityBlockingQueueTest.java
@@ -1,2 +1,17 @@
-package com.lzy.learning.demo;public class PriorityBlockingQueueTest {
+package com.lzy.learning.demo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.stream.IntStream;
+
+
+public class PriorityBlockingQueueTest {
+    @Test
+    void putTest() {
+        PriorityBlockingQueue<Integer> queue = new PriorityBlockingQueue<>();
+        IntStream.range(0, 10).forEach(i -> queue.put(new Random().nextInt(100)));
+        System.out.println(queue);
+    }
 }
