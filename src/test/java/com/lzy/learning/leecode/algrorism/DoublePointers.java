3d8ea2ca64f368a169117ca6550511e949f22915
Zhiyong Lv <zhiyong.lv@veritas.com>
2023-07-08 21:47:40 +0800

    commit DoublePointers related

---
+++ src/test/java/com/lzy/learning/leecode/algrorism/DoublePointers.java
@@ -0,0 +1,214 @@
+package com.lzy.learning.leecode.algrorism;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class DoublePointers {
+    public static void main(String[] args) {
+        System.out.printf("result is %s\n", new Solution15().threeSum(new int[] {-1, 0, 1, 2, -1, -4}));
+    }
+}
+
+/**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * <p>
+ * 注意： 答案中不可以包含重复的三元组。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * <p>
+ * 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+ */
+class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; ) {
+            for (int j = i + 1, k = nums.length - 1; j < k; ) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    do {j++;} while (j < k && nums[j - 1] == nums[j]);
+                } else {
+                    do {k--;} while (j < k && nums[k] == nums[k + 1]);
+                }
+            }
+            do {i++;} while (i < nums.length - 2 && nums[i - 1] == nums[i]);
+        }
+        return result;
+    }
+}
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * <p>
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 说明:
+ * <p>
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * <p>
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ * <p>
+ * 你可以想象内部操作如下:
+ * <p>
+ * // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+ * int len = removeElement(nums, val);
+ * <p>
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,2,2,3], val = 3
+ * 输出：2, nums = [2,2]
+ * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,1,2,2,3,0,4,2], val = 2
+ * 输出：5, nums = [0,1,4,0,3]
+ * 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 100
+ * 0 <= nums[i] <= 50
+ * 0 <= val <= 100
+ * Related Topics
+ * 数组
+ * 双指针
+ */
+class Solution27 {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[slow++] = nums[i];
+            }
+        }
+        return slow;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+/**
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 示例 2：
+ * <p>
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ */
+class Solution19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fakeHead = new ListNode(-1, head);
+        ListNode slowNode = fakeHead;
+        int slow = -1, fast = 0;
+        while (Objects.nonNull(head.next)) {
+            if (fast - slow == n) {
+                slow++;
+                slowNode = slowNode.next;
+            }
+            fast++;
+            head = head.next;
+        }
+
+        slowNode.next = slowNode.next.next;
+
+        return fakeHead.next;
+    }
+}
+
+/**
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ * <p>
+ * 图示两个链表在节点 c1 开始相交：
+ * <p>
+ * <p>
+ * <p>
+ * 题目数据 保证 整个链式结构中不存在环。
+ * <p>
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>
+ * 自定义评测：
+ * <p>
+ * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+ * <p>
+ * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+ * listA - 第一个链表
+ * listB - 第二个链表
+ * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+ * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+ * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+ */
+class Solution160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = getLength(headA);
+        int lenB = getLength(headB);
+
+        ListNode longList, shortList;
+        int step = Math.abs(lenA - lenB);
+        if (lenA > lenB) {
+            longList = headA;
+            shortList = headB;
+        } else {
+            longList = headB;
+            shortList = headA;
+        }
+
+        for (int i = 0; i < step; i++) {
+            longList = longList.next;
+        }
+
+        while (Objects.nonNull(longList) && Objects.nonNull(shortList)) {
+            if (longList == shortList) {
+                return longList;
+            }
+            longList = longList.next;
+            shortList = shortList.next;
+        }
+        return null;
+    }
+
+    private int getLength(ListNode head) {
+        ListNode preNode = new ListNode(-1, head);
+        int length = 0;
+
+        while (Objects.nonNull(preNode.next)) {
+            length++;
+            preNode = preNode.next;
+        }
+        return length;
+    }
+}
